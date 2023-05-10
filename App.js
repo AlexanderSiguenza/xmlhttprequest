909d15f8b17aba1c054950fd1c16c136839ad272
ALEXANDER ALBERTO SIGUENZA CAMPOS <alexander.siguenzac@claro.com.sv>
2023-05-10 02:03:52 -0600

    primer version

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
 
 export default function App() {
+  const [dogImage, setDogImage] = useState('');
+  const handleButtonClick = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        setDogImage(response.message);
+      }
+    };
+    xhr.send();
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Random Dog Image:</Text>
+      <Button title="Get Random Dog" onPress={handleButtonClick} />
+      {dogImage ? (
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: dogImage }} style={styles.image} />
+        </View>
+      ) : null}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  imageContainer: {
+    marginTop: 20,
+    width: '100%',
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
   },
 });
